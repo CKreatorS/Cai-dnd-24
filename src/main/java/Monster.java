dbd1989725bf6784e04c8caa2c65ae46ba6401a8
cmorales <caitlyn.morales424@gmail.com>
2024-11-15 19:56:10 -0500

    monsters

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,5 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Monster {
+
+    public String names;
+    public String difficulty;
+    public int level;
+    public int HP;
+
+    public Monster(){
+
+    }
+
+
+    // public void generateMonster() {
+
+    // }
     
 }
