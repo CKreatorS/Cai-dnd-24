8355940a1fbca48a611267506ee22dabc94d5b4a
cmorales <caitlyn.morales424@gmail.com>
2024-11-17 00:19:06 -0500

    augh

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 public class Monster {
 
     public String names;
-    public String difficulty;
-    public int level;
+    public int difficulty;
     public int HP;
 
-    public Monster(){
+    public Monster() {
+
+        this.names = names;
+        this.difficulty = difficulty; 
+
+        int[] difficulty = new int[]{5, 7, 9, 10};
+        shuffle(difficulty);
+
+        int[] HP = new int[]{10, 20, 25, 15}; 
+        shuffle(HP);
 
     }
 
+    public void generateMonster() {
 
-    // public void generateMonster() {
-
-    // }
+    }
     
+    private void shuffle(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            
+            // Swap elements
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
 }
